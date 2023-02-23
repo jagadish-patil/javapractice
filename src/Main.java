2461f5b752a6c484b9b9f365926a06f08d444484
jagadishpatil <jagadishpatil231@gmail.com>
2023-02-23 22:49:44 +0530

    intro and variables

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("I am a programmer");
+        System.out.println("Hello World");
+        System.out.println("I love Java");
+    }
+}
