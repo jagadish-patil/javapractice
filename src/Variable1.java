4f72f503d808140cd881714e1702be1dd97b02e3
jagadishpatil <jagadishpatil231@gmail.com>
2023-02-23 22:53:08 +0530

    intro and variables

---
+++ src/Variable1.java
@@ -0,0 +1,8 @@
+public class Variable1 {
+    public static void main(String[] args) {
+        int num = 10;
+        int num2 = 5;
+        int num_3 = 2;
+//        int 4num = 100;       // not correct
+    }
+}
